b42393fb3350d2227292bfbeeb072eae0fc0c10b
Kevin Swiber <kswiber@gmail.com>
2021-10-10 10:46:44 -0700

    Adding site metadata.

--- gatsby-node.js
+++ gatsby-node.js
@@ -15,19 +15,47 @@ const yaml = require("js-yaml");
 const publicPath = path.resolve("./public");
 const apiPath = path.join(publicPath, "api");
 const gatewaysPath = path.join(apiPath, "gateways");
+const contentPath = "./src/content/gateways";
 
 const apiResponses = {};
 
-exports.createPages = ({ actions }) => {
+exports.createPages = async ({ actions, graphql }) => {
   const { createPage } = actions;
-  const docs = fs.readdirSync("./src/content");
+  const docs = fs.readdirSync(contentPath);
   const converter = new Converter();
+  const result = await graphql(
+    `
+      {
+        allSite {
+          edges {
+            node {
+              siteMetadata {
+                siteUrl
+              }
+            }
+          }
+        }
+      }
+    `
+  );
+  console.log(result);
+  const siteUrl = result.data.allSite.edges[0].node.siteMetadata.siteUrl;
+  const apiRoot = {
+    api: {
+      title: "API Gateway Research",
+      links: {
+        author: "kevin.swiber@postman.com",
+        describedBy: `${siteUrl}/openapi.yaml`,
+      },
+    },
+    resources: {},
+  };
 
   docs.forEach((doc) => {
     const parts = doc.split(".");
     const joined = parts.slice(0, parts.length - 1).join(".");
 
-    const content = fs.readFileSync(`./src/content/${doc}`);
+    const content = fs.readFileSync(path.join(contentPath, doc));
     const context = yaml.load(content);
 
     apiResponses[joined] = JSON.stringify(context);
