1020981b4e48eca1453263374d52759743294b1c
Kevin Swiber <kswiber@gmail.com>
2021-10-11 12:03:43 -0700

    Moving YAML to source content, moving Siren API generation to local plugin.

--- src/templates/platforms.js
+++ src/templates/platforms.js
@@ -1,20 +1,27 @@
 import React from "react";
-import { Link } from "gatsby";
-const gatewayTemplate = (props) => {
-  const { pageContext } = props;
-  const { name, description, links } = pageContext;
+import { Converter } from "showdown";
+import sanitizeHtml from "sanitize-html";
+import { graphql, Link } from "gatsby";
+
+const gatewayTemplate = ({ data }) => {
+  const { properties, links } = data.allYaml.edges[0].node;
+  const { name, description } = properties;
+
+  const converter = new Converter();
+  const descriptionHtml = converter.makeHtml(description);
+  const sanitizedDescription = sanitizeHtml(descriptionHtml);
+
   return (
     <div style={{ maxWidth: `960px`, margin: `1.45rem` }}>
       <h2>{name}</h2>
       <ul>
-        <div dangerouslySetInnerHTML={{ __html: description }} />
+        <div dangerouslySetInnerHTML={{ __html: sanitizedDescription }} />
       </ul>
       <ul>
-        {Object.keys(links).map((key, index) => {
-          const item = links[key];
+        {links.forEach((item, index) => {
           return (
             <li key={`link_${index}`}>
-              <Link to={item.url}>{item.title}</Link>
+              <Link to={item.href}>{item.title}</Link>
             </li>
           );
         })}
@@ -23,3 +30,36 @@ const gatewayTemplate = (props) => {
   );
 };
 export default gatewayTemplate;
+export const query = graphql`
+  query ($id: String!) {
+    allYaml(filter: { id: { eq: $id } }) {
+      edges {
+        node {
+          class
+          properties {
+            name
+            provider
+            license
+            researchDate
+            description
+            notes
+            platformCapabilities
+            downstreamProtocols
+            gatewayCapabilities
+            customCodeSupport
+            deliveryModels
+            policies {
+              capabilities
+              name
+            }
+          }
+          links {
+            href
+            rel
+            title
+          }
+        }
+      }
+    }
+  }
+`;
