6c68c8c588f0fde6081e869f756e93bf29fdd4e9
Kevin Swiber <kswiber@gmail.com>
2021-10-11 19:39:53 -0700

    Removing extraneous links from header.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -234,337 +234,17 @@ class Header extends React.Component {
 
   render() {
     //const { refresh, hasInput, beta, visibleHelloBar, cookie } = this.state;
-    const { visibleHelloBar } = this.state;
     return (
       <>
-        <nav className="navbar-v6 navbar navbar-expand-lg navbar-light bg-light nav-primary">
+        <nav className="navbar-v6 navbar navbar-expand-lg navbar-light bg-light nav-secondary blurred-container">
           <a className="navbar-brand" href="https://www.postman.com">
             <div className="navbar-logo-container">
               <img src={postmanLogo} alt="Postman" width="32" height="32" />
             </div>
           </a>
-          <button
-            onClick={() => {
-              this.showTargetElement();
-              this.hideTargetElement();
-            }}
-            id="globalNav"
-            className="mobile-sign-in navbar-toggler"
-            data-test="mobileNavToggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon">
-              <div
-                id="icon-wrap-one"
-                className="icon-bar"
-                aria-expanded="false"
-              >
-                <span />
-                <span />
-                <span />
-                <span />
-              </div>
-            </span>
-          </button>
-          <div
-            id="navbarSupportedContent"
-            className={`collapse navbar-collapse${
-              !visibleHelloBar ? " noBar" : ""
-            }`}
-          >
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="##"
-                  id="navbarDropdownMenuLink"
-                  data-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Product
-                  <svg
-                    className="arrow-icon"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="12"
-                    height="12"
-                    viewBox="0 0 12 12"
-                    fill="#6b6b6b"
-                  >
-                    <g>
-                      <path d="M10.375,3.219,6,6.719l-4.375-3.5A1,1,0,1,0,.375,4.781l5,4a1,1,0,0,0,1.25,0l5-4a1,1,0,0,0-1.25-1.562Z" />
-                    </g>
-                  </svg>
-                </a>
-                <div
-                  className="dropdown-menu"
-                  aria-labelledby="navbarDropdownMenuLink"
-                >
-                  <a
-                    className="dropdown-item"
-                    href="https://www.postman.com/product/what-is-postman/"
-                  >
-                    What is Postman?
-                  </a>
-                  <a
-                    className="dropdown-item"
-                    href="https://www.postman.com/product/api-repository/"
-                  >
-                    API repository
-                  </a>
-                  <a
-                    className="dropdown-item"
-                    href="https://www.postman.com/product/tools/"
-                  >
-                    Tools
-                  </a>
-                  <a
-                    className="dropdown-item"
-                    href="https://www.postman.com/product/intelligence/"
-                  >
-                    Intelligence
-                  </a>
-                  <a
-                    className="dropdown-item"
-                    href="https://www.postman.com/product/workspaces/"
-                  >
-                    Workspaces
-                  </a>
-                  <a
-                    className="dropdown-item"
-                    href="https://www.postman.com/product/integrations/"
-                  >
-                    Integrations
-                  </a>
-                  <a
-                    className="dropdown-item app-cta"
-                    href="https://www.postman.com/downloads/"
-                  >
-                    Get started free →
-                  </a>
-                </div>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="https://www.postman.com/pricing/">
-                  Pricing
-                </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="##"
-                  id="navbarDropdownMenuLink"
-                  data-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Enterprise
-                  <svg
-                    className="arrow-icon"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="12"
-                    height="12"
-                    viewBox="0 0 12 12"
-                    fill="#6b6b6b"
-                  >
-                    <g>
-                      <path d="M10.375,3.219,6,6.719l-4.375-3.5A1,1,0,1,0,.375,4.781l5,4a1,1,0,0,0,1.25,0l5-4a1,1,0,0,0-1.25-1.562Z" />
-                    </g>
-                  </svg>
-                </a>
-                <div
-                  className="dropdown-menu"
-                  aria-labelledby="navbarDropdownMenuLink"
-                >
-                  <a
-                    className="dropdown-item"
-                    href="https://www.postman.com/postman-enterprise/"
-                  >
-                    Postman Enterprise
-                  </a>
-                  <a
-                    className="dropdown-item"
-                    href="https://www.postman.com/case-studies/"
-                  >
-                    Enterprise case studies
-                  </a>
-                  <a
-                    className="dropdown-item"
-                    href="https://www.postman.com/company/contact-sales/"
-                  >
-                    Contact sales
-                  </a>
-                </div>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="##"
-                  id="navbarDropdownMenuLink"
-                  data-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Resources and support
-                  <svg
-                    className="arrow-icon"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="12"
-                    height="12"
-                    viewBox="0 0 12 12"
-                    fill="#6b6b6b"
-                  >
-                    <g>
-                      <path d="M10.375,3.219,6,6.719l-4.375-3.5A1,1,0,1,0,.375,4.781l5,4a1,1,0,0,0,1.25,0l5-4a1,1,0,0,0-1.25-1.562Z" />
-                    </g>
-                  </svg>
-                </a>
-                <div
-                  className="dropdown-menu"
-                  aria-labelledby="navbarDropdownMenuLink"
-                >
-                  <div className="row dropdown-col-menu">
-                    <div className="col-sm-6 col-md-4 dropdown-col">
-                      <h6 className="dropdown-header">Learning</h6>
-                      <a
-                        className="dropdown-item"
-                        href="/docs/getting-started/introduction/"
-                      >
-                        Docs
-                      </a>
-                      <a
-                        className="dropdown-item"
-                        href="https://www.postman.com/webinars/"
-                      >
-                        Webinars
-                      </a>
-                      <a
-                        className="dropdown-item"
-                        href="https://www.postman.com/events/breaking-changes/"
-                      >
-                        Breaking Changes show
-                      </a>
-                      <a
-                        className="dropdown-item"
-                        href="https://www.postman.com/events/postman-space-camp/"
-                      >
-                        Postman Space Camp
-                      </a>
-                      <a
-                        className="dropdown-item"
-                        href="https://www.postman.com/case-studies/"
-                      >
-                        Case studies
-                      </a>
-                    </div>
-                    <div className="col-sm-6 col-md-4 dropdown-col">
-                      <h6 className="dropdown-header">Community and events</h6>
-                      <a
-                        className="dropdown-item"
-                        href="https://blog.postman.com/"
-                      >
-                        Blog
-                      </a>
-                      <a
-                        className="dropdown-item"
-                        href="https://www.postman.com/community/"
-                      >
-                        Community
-                      </a>
-                      <a
-                        className="dropdown-item"
-                        href="https://www.postman.com/company/student-program/"
-                      >
-                        Student program
-                      </a>
-                      <a
-                        className="dropdown-item"
-                        href="https://www.postman.com/company/student-program/student-summit/"
-                      >
-                        Student Summit
-                      </a>
-                      <a
-                        className="dropdown-item"
-                        href="https://www.postman.com/postman-galaxy/"
-                      >
-                        Postman Galaxy
-                      </a>
-                      <a
-                        className="dropdown-item"
-                        href="https://www.postman.com/company/nonprofit/"
-                      >
-                        Postman for Nonprofits
-                      </a>
-                      <a
-                        className="dropdown-item"
-                        href="https://store.getpostman.com/?_ga=2.81312663.1499710862.1631566802-1987217384.1631320816"
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        Postman swag
-                      </a>
-                    </div>
-                    <div className="col-sm-6 col-md-4 dropdown-col">
-                      <h6 className="dropdown-header">Support</h6>
-                      <a
-                        className="dropdown-item"
-                        href="https://www.postman.com/support/"
-                      >
-                        Support Center
-                      </a>
-                      <a
-                        className="dropdown-item"
-                        href="https://www.postman.com/support/resellers-support/"
-                      >
-                        Reseller support
-                      </a>
-                      <a
-                        className="dropdown-item"
-                        href="https://status.postman.com/"
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        Postman status
-                      </a>
-                      <a
-                        className="dropdown-item"
-                        href="https://www.postman.com/downloads/release-notes/"
-                      >
-                        Release notes
-                      </a>
-                      <a
-                        className="dropdown-item"
-                        href="https://www.postman.com/company/contact-us/"
-                      >
-                        Contact us
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="https://www.postman.com/explore">
-                  Explore
-                </a>
-              </li>
-            </ul>
-            {/*}
-            <div className="form-inline my-2 my-lg-0">
-              <LoginCheck
-                cookie={cookie}
-                beta={beta}
-                className="pingdom-transactional-check__sign-in-button"
-              />
-            </div>
-          */}
-          </div>
-        </nav>
-        <nav className="navbar-v6 navbar navbar-expand-lg navbar-light bg-light nav-secondary blurred-container">
           <a className="navbar-brand" href="/">
             <span id="learning-center-home-link" className="nav-link uber-nav">
-              Learning Center
+              API Gateways
               <span className="sr-only">(current)</span>
             </span>
           </a>
@@ -596,6 +276,7 @@ class Header extends React.Component {
               </div>
             </span>
           </button>
+          {/*}
           <div
             className="collapse navbar-collapse"
             id="navbarSupportedContentBottom"
@@ -626,8 +307,7 @@ class Header extends React.Component {
                 </a>
               </li>
             </ul>
-            {/* Aloglia Widgets */}
-            {/*}
+            {/* Aloglia Widgets *}
             <div className="form-inline header__search">
               <label htmlFor="search-lc" />
               <svg
@@ -697,8 +377,8 @@ class Header extends React.Component {
                 </InstantSearch>
               </ClickOutHandler>
             </div>
-              */}
           </div>
+              */}
         </nav>
       </>
     );
