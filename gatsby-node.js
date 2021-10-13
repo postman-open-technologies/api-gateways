47e8e269fd7fdc84fa6f4b67370c616a4aa22e6d
Kevin Swiber <kswiber@gmail.com>
2021-10-12 18:43:29 -0700

    Fixing redirect and disappearing content issue.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,17 @@
-if (process.env.NODE_ENV === "development") {
+const isDevelopment = process.env.NODE_ENV === "development";
+
+if (isDevelopment) {
   // lol, tryna override the default type so
   // `gatsby develop` serves my API right
   const mime = require("./node_modules/send/node_modules/mime");
   mime.default_type = "application/vnd.siren+json";
 }
+
+exports.createPages = ({ actions }) => {
+  const { createRedirect } = actions;
+  createRedirect({
+    fromPath: "/",
+    toPath: "/gateways/",
+    redirectInBrowser: isDevelopment,
+  });
+};
