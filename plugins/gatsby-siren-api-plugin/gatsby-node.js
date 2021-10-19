5e8763ef917f31e1f4a4dfc96d01146e8a5a8717
Kevin Swiber <kswiber@gmail.com>
2021-10-19 15:35:26 -0700

    Consolidating all Siren API plugin logic to createPages.  Changing index.html to index.json to see if Netlify supports that.

--- plugins/gatsby-siren-api-plugin/gatsby-node.js
+++ plugins/gatsby-siren-api-plugin/gatsby-node.js
@@ -5,41 +5,6 @@ const yaml = require("js-yaml");
 const publicPath = path.resolve("./public");
 const apiPath = path.join(publicPath, "api");
 
-const apiResponses = {};
-
-exports.onCreateNode = ({ node }) => {
-  if (node.sourceInstanceName !== "source-yaml") {
-    return;
-  }
-
-  if (node.internal.type === "Directory" && node.relativePath !== "") {
-    const dirPath = path.join(apiPath, node.relativePath);
-    apiResponses[node.relativePath] = {};
-
-    if (!fs.existsSync(dirPath)) {
-      fs.mkdirSync(dirPath, { recursive: true });
-    }
-  }
-
-  if (node.internal.type === "File") {
-    const filePath = path.join(apiPath, node.relativeDirectory, node.name);
-
-    const json = yaml.load(node.internal.content);
-    const value = Object.assign({}, json);
-    if (!value.links) {
-      value.links = [];
-    }
-    value.links.push({
-      rel: ["self"],
-      href: `/api/${path.join(node.relativeDirectory, node.name)}`,
-    });
-    if (fs.existsSync(publicPath)) {
-      fs.writeFileSync(filePath, JSON.stringify(value));
-    }
-    apiResponses[node.relativeDirectory][node.name] = value;
-  }
-};
-
 exports.createPages = async ({ actions, graphql }, pluginOptions) => {
   const templates = pluginOptions.templates || [];
   const typeName = pluginOptions.typeName || "allYaml";
@@ -55,6 +20,10 @@ exports.createPages = async ({ actions, graphql }, pluginOptions) => {
               ... on File {
                 name
                 relativeDirectory
+                relativePath
+                internal {
+                  content
+                }
               }
             }
           }
@@ -63,14 +32,44 @@ exports.createPages = async ({ actions, graphql }, pluginOptions) => {
     }
   `);
 
+  const apiResponses = {};
   data[typeName].edges.forEach((edge) => {
-    const klasses = edge.node.class;
+    const node = edge.node;
+    const klasses = node.class;
     if (klasses.length === 0) {
       return;
     }
 
+    const dirPath = path.join(apiPath, node.parent.relativeDirectory);
+    if (!fs.existsSync(dirPath)) {
+      fs.mkdirSync(dirPath, { recursive: true });
+    }
+    apiResponses[node.parent.relativeDirectory] = [];
+
+    const filePath = path.join(
+      apiPath,
+      node.parent.relativeDirectory,
+      node.parent.name
+    );
+
+    const json = yaml.load(node.parent.internal.content);
+    const value = Object.assign({}, json);
+    if (!value.links) {
+      value.links = [];
+    }
+    value.links.push({
+      rel: ["self"],
+      href: `/api/${path.join(
+        node.parent.relativeDirectory,
+        node.parent.name
+      )}`,
+    });
+
+    apiResponses[node.parent.relativeDirectory].push(value);
+    fs.writeFileSync(filePath, JSON.stringify(value));
+
     const klass = klasses[0];
-    const routePath = `${edge.node.parent.relativeDirectory}/${edge.node.parent.name}`;
+    const routePath = `${node.parent.relativeDirectory}/${node.parent.name}`;
 
     const template = templates[klass];
     if (!template) {
@@ -80,37 +79,24 @@ exports.createPages = async ({ actions, graphql }, pluginOptions) => {
     actions.createPage({
       path: `/${routePath}/`,
       component: template,
-      context: { id: edge.node.id },
+      context: { id: node.id },
     });
   });
-};
 
-exports.onPostBootstrap = () => {
   for ([dir, responses] of Object.entries(apiResponses)) {
     const dirPath = path.join(apiPath, dir);
-    if (!fs.existsSync(dirPath)) {
-      fs.mkdirSync(dirPath, { recursive: true });
-    }
-    const values = [];
-    for ([key, value] of Object.entries(responses)) {
-      const self = path.join(dirPath, key);
-      values.push(value);
-      fs.writeFileSync(self, JSON.stringify(value));
-    }
-    if (values.length > 0) {
-      fs.writeFileSync(
-        path.join(dirPath, "index.html"),
-        JSON.stringify({
-          class: [dir],
-          entities: values,
-          links: [
-            {
-              rel: ["self"],
-              href: `/api/${dir}/`,
-            },
-          ],
-        })
-      );
-    }
+    fs.writeFileSync(
+      path.join(dirPath, "index.json"),
+      JSON.stringify({
+        class: [dir],
+        entities: responses,
+        links: [
+          {
+            rel: ["self"],
+            href: `/api/${dir}/`,
+          },
+        ],
+      })
+    );
   }
 };
